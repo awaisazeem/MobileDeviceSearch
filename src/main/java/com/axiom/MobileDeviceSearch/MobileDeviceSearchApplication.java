bcd3f7dec5c842ff374bdcb8e74b544a8e3c2e31
Awais <awaisazeempk@gmail.com>
2020-08-31 23:15:27 +0500

    initial commit

---
+++ src/main/java/com/axiom/MobileDeviceSearch/MobileDeviceSearchApplication.java
@@ -0,0 +1,13 @@
+package com.axiom.MobileDeviceSearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileDeviceSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobileDeviceSearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/axiom/MobileDeviceSearch/MobileDeviceSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.axiom.MobileDeviceSearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileDeviceSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
